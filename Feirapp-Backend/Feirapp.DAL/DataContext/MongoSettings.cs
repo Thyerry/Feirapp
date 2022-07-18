be6c3829676bbfbb6a4abf2090579e321e9e722f
thyerry <nunes.thyerry@outlook.com>
2022-07-18 12:12:43 -0300

    #66 Test the build on pull request

--- Feirapp-Backend/Feirapp.DAL/DataContext/MongoSettings.cs
+++ Feirapp-Backend/Feirapp.DAL/DataContext/MongoSettings.cs
@@ -2,6 +2,6 @@ namespace Feirapp.DAL.DataContext;
 
 public class MongoSettings
 {
-    public string ConnectionString { get; set; }
-    public string DatabaseName { get; set; }
+    public string ConnectionString { get; set; } = string.Empty;
+    public string DatabaseName { get; set; } = string.Empty;
 }
