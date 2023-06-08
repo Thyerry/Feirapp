481a0caa84f0ec9ae947aa665392c61799a19207
thyerry <thyerry.nunes@gmail.com>
2023-06-08 12:48:19 -0300

    Create a test that raises a mongo container and do operations in it

--- Feirapp-Backend/Feirapp.UnitTests/Fixtures/DockerComposeTestBase.cs
+++ Feirapp-Backend/Feirapp.UnitTests/Fixtures/DockerComposeTestBase.cs
@@ -1,33 +1,66 @@
 ﻿using Ductus.FluentDocker.Builders;
 using Ductus.FluentDocker.Extensions;
 using Ductus.FluentDocker.Services;
+using Feirapp.DAL.DataContext;
+using Feirapp.Domain.Models;
+using Microsoft.Extensions.Options;
 using System;
+using System.Diagnostics;
+using MongoDB.Driver;
 using Xunit;
+using FluentAssertions;
 
-namespace Feirapp.UnitTests.Fixtures
+namespace Feirapp.UnitTests.Fixtures;
+
+public class DockerComposeTestBase : IDisposable
 {
-    public class DockerComposeTestBase : IDisposable
-    {
-        [Fact]
-        public void docker()
-        {
-            var ok = "ok";
-            using (var container = new Builder()
-                .UseContainer()
-                .UseImage("mongo:latest")
-                .ExposePort(27019)
-                .WaitForPort("27019/tcp", 3000)
-                .Build()
-                .Start())
-            {
-                var config = container.GetConfiguration(true);
-                Assert.Equal(ServiceRunningState.Running, config.State.ToServiceState());
-            }
-        }
+    private readonly IContainerService _container;
 
-        public void Dispose()
+    public DockerComposeTestBase()
+    {
+        _container = new Builder()
+            .UseContainer()
+            .UseImage("mongo:latest")
+            .ExposePort(27017, 27017)
+            .WaitForPort("27017/tcp", 3000)
+            .WithName("integrationTestsContainer")
+            .Build()
+            .Start();
+    }
+
+    [Fact]
+    public async void mini_integration_test()
+    {
+        var mongoSettings = new MongoSettings()
         {
-            Console.WriteLine("YEAH, I'M THINKING DISPOSED 🤬🤬🤬");
-        }
+            ConnectionString = "mongodb://localhost:27017",
+            DatabaseName = "Feirapp",
+        };
+        var mongoContext = new MongoFeirappContext(new OptionsConfigurationMock<MongoSettings>(mongoSettings));
+
+        var collection = mongoContext.GetCollection<GroceryItem>(nameof(GroceryItem));
+        var expected = new GroceryItem { Name = "thyerry" };
+
+        await collection.InsertOneAsync(expected);
+
+        var actual = (await collection.FindAsync(g => g.Name.Contains(expected.Name))).FirstOrDefault();
+
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    public void Dispose()
+    {
+        Debug.WriteLine("YEAH, I'M THINKING I'M DISPOSED 😡");
+        _container.Dispose();
+    }
+}
+
+public class OptionsConfigurationMock<T> : IOptions<T> where T : class
+{
+    public T Value { get; }
+
+    public OptionsConfigurationMock(T value)
+    {
+        Value = value;
     }
 }
--- Feirapp-Backend/Feirapp.UnitTests/IntegrationTest/TestGroceryItemRepository.cs
+++ Feirapp-Backend/Feirapp.UnitTests/IntegrationTest/TestGroceryItemRepository.cs
@@ -1,13 +1,5 @@
-﻿using System.Threading.Tasks;
-using Xunit;
-
-namespace Feirapp.UnitTests.IntegrationTest;
+﻿namespace Feirapp.UnitTests.IntegrationTest;
 
 public class TestGroceryItemRepository
 {
-    [Fact]
-    public void Oi()
-    {
-
-    }
 }
