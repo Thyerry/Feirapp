bc5e0d429a0c6b4d2e734f0a31c18cb526c6f42f
Thyérry <thyerry.nunes@gmail.com>
2024-11-01 15:47:36 -0300

    Create api response factory methods.

---
+++ Feirapp-Backend/Feirapp.API/Helpers/Response/ApiResponseFactory.cs
@@ -0,0 +1,19 @@
+namespace Feirapp.API.Helpers.Response;
+
+public static class ApiResponseFactory
+{
+    public static ApiResponse<T> Success<T>(T data, string message = "Request successful")
+    {
+        return new ApiResponse<T>("success", message, data, null);
+    }
+    
+    public static ApiResponse<T> Failure<T>(string message, List<string>? errors = null)
+    {
+        return new ApiResponse<T>("failure", message, default, errors ?? []);
+    }
+    
+    public static ApiResponse<T> Error<T>(string message, List<string>? errors = null)
+    {
+        return new ApiResponse<T>("error", message, default, errors ?? []);
+    }
+}
